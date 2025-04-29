d23a8f8ac0a8d6523de221e9b74c5bc01d453013
Maria Dumas <mdumas2@rrc.ca>
2025-04-29 16:37:22 -0500

    Added validations, associations and comments to the Genre Model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,9 @@
 class Genre < ApplicationRecord
+  # Validates that the genre must have a name before it can be saved.
   validates :name, presence: true
+
+  # Sets up a one-to-many relationship with the BookGenre join model.
   has_many :book_genres
+  # Sets up a many-to-many relationship with books through the book_genres table.
   has_many :books, through: :book_genres
 end
