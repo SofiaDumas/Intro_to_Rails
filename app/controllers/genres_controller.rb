3eb6e81b258dac9e0ca61b30aca618efcdc91a13
Maria Dumas <mdumas2@rrc.ca>
2025-04-29 16:24:36 -0500

    Fixing and adding comments to the Genre Controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,10 +1,14 @@
 class GenresController < ApplicationController
+  # This controller handles displaying the list of genres
+  # Displays a paginated list of all genres, sorted alphabetically.
   def index
-    @genres = Genre.all
+    # Fetch all genres, in alphabetical order and paginated by 5.
     @genres = Genre.order(:name).page(params[:page]).per(5)
   end
 
+  # Displays details of a specific genre
   def show
+    # Find the genre by its id
     @genre = Genre.find(params[:id])
   end
 end
