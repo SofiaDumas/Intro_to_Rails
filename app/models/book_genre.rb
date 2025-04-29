d2eec6f1012e228898c366bab3d2c1930620311b
Maria Dumas <mdumas2@rrc.ca>
2025-04-29 16:32:18 -0500

    Added associations and comments to the BookGenre Model

--- app/models/book_genre.rb
+++ app/models/book_genre.rb
@@ -1,4 +1,6 @@
 class BookGenre < ApplicationRecord
+  # Sets up the associations that the BookGenre record belongs to a single book
+  # and belongs to a single genre.
   belongs_to :book
   belongs_to :genre
 end
