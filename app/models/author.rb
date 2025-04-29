25acc21f3a1ae9858e0e78add1f637c5ef1369ff
Maria Dumas <mdumas2@rrc.ca>
2025-04-29 16:27:06 -0500

    Added validations, associations and comments to the Author Model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
+  # Validate that the author's name is present
   validates :name, presence: true
+  # Sets up a one-to-many association: An author can have many books
   has_many :books
 end
