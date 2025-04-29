85eca482da27d98c33773c9ca08fdb54dbdeaf23
Maria Dumas <mdumas2@rrc.ca>
2025-04-29 16:20:06 -0500

    Fixing and adding comments to the Books Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
-  # layout "application"
+  # This controller handles displaying the list of books
   def index
+    # Displays a paginated list of all books
+    # Fi a search query is provided, it filter the books by title.
     if params[:query].present?
+      # Results are order alphabetically by title and paginated by 30.
       @books = Book.where("title LIKE ?", "%#{params[:query]}%").order(:title).page(params[:page]).per(30)
     else
-      @books = Book.all.includes(:author, :publisher, :genres)
-      @books = Book.order(:title).page(params[:page]).per(30)
+      # If no query is done, retrieve all books
+      @books = Book.includes(:author, :genres).order(:title).page(params[:page]).per(30)
     end
   end
 
+  # Displays the details fo a specific book, including related author and genre.
   def show
+    # Find the book by its id.
     @book = Book.find(params[:id])
   end
 end
