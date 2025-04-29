3f64ab7aceb8568048d704dd3d81620904c59144
Maria Dumas <mdumas2@rrc.ca>
2025-04-29 16:12:34 -0500

    Fixing and adding comments to the Authors Controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,18 @@
 class AuthorsController < ApplicationController
+  # This Controller lists all authors, optionally filtered by a search query.
   def index
     if params[:query].present?
+      # If a search query is provided, find all the authors whose names contain the query
+      # Tne order by name and paginate by 10 authors.
       @authors = Author.where("name LIKE ?",  "%#{params[:query]}%").order(:name).page(params[:page]).per(10)
     else
-      @authors = Author.all
+      # If no retrieve all authors in alphabetical order and paginated.
       @authors = Author.order(:name).page(params[:page]).per(10)
     end
   end
 
   def show
+    # Finds a single author using the id
     @author = Author.find(params[:id])
   end
 end
